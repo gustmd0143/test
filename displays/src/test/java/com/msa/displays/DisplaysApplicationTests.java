f59f22f6f4ecf8c76e380a1abd43b3b92a0c9c8b
gustmd38 <gustmd38@sk.com>
2021-09-27 17:14:10 +0900

    sample project

---
+++ displays/src/test/java/com/msa/displays/DisplaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.displays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
